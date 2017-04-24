69a2d5d4e03ea101b51c6cf6703a61134a1fe3d4
unknown <mateusz.strzalko@wp.pl>
2017-04-24 21:15:29 +0200

    oop

--- src/zajecia/oop/User.java
+++ src/zajecia/oop/User.java
@@ -4,4 +4,16 @@ package zajecia.oop;
  * Created by RENT on 2017-04-24.
  */
 public class User {
+    public String firstName;
+    public String lastName;
+    public String phoneNumber;
+    public String address;
+    public int age;
+
+public void display() {
+        System.out.println(firstName + " " +lastName + " age: " + age);
+        System.out.println("Phone number: " + phoneNumber);
+       address.display();
+
+        }
 }
--- src/zajecia/oop/bookstore/Bookstore.java
+++ src/zajecia/oop/bookstore/Bookstore.java
@@ -29,7 +29,7 @@ public void showBooks() {
 }
 public int getNumberOfBooks(String author) {
     for (int i = 0; i < size; i++) {
-        
+
     }
 }
 
